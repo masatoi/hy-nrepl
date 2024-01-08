207837a616cb5936107b565849ac8c6e41ab3718
Satoshi Imai <satoshi.imai@gmail.com>
2024-01-08 19:48:25 +0900

    Update dependency

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import find_packages, setup
 
 setup(name="HyREPL",
-      version="0.2.0",
-      install_requires=['hy==0.27.0', 'hyrule==0.4.0', 'toolz'],
+      version="0.2.1",
+      install_requires=['hy==0.28.0', 'hyrule==0.5.0', 'toolz'],
       dependency_links=[
-          'https://github.com/hylang/hy/archive/master.zip#egg=hy-0.27.0',
+          'https://github.com/hylang/hy/archive/master.zip#egg=hy-0.28.0',
       ],
       packages=find_packages(exclude=['tests']),
       package_data={
