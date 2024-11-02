afb2c53b9f5e5e373df8ae09b1aaec17a2bbd34f
Satoshi Imai <wiz@M3MBA.local>
2024-11-02 19:29:33 +0900

    Hy specific setting, so it is unnecessary

--- conftest.py
+++ conftest.py
@@ -1,13 +1,8 @@
-import os
 from pathlib import Path
 
 import hy, pytest
 
 NATIVE_TESTS = Path.cwd() / "tests"
-
-# https://github.com/hylang/hy/issues/2029
-os.environ.pop("HYSTARTUP", None)
-
 
 def pytest_collect_file(file_path, parent):
     if (file_path.suffix == ".hy" and NATIVE_TESTS in file_path.parents
